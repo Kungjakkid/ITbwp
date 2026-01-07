43895183d4c13181dd8b79be856c15d3c9f1ba03
Kungjakkid <148857819+Kungjakkid@users.noreply.github.com>
2026-01-07 11:51:27 +0700

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// config.js
-const SUPABASE_URL = 'https://yqlyxzowfbowznpzapxf.supabase.co'; 
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxbHl4em93ZmJvd3pucHphcHhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMwMTc3NDEsImV4cCI6MjA3ODU5Mzc0MX0.ZhJAq0mt3LAamCZlBGux_fwhyQIlOab_0BFsaWubHko';
-
-// ประกาศตัวแปร Global ให้ทุกไฟล์เรียกใช้ได้เลย
-const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
